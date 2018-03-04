366675ff79444dd13a5c4d378b03d1cfbc13caaf
somainer <somainer@gmail.com>
2018-03-04 16:27:25 +0800

    InmCal + Brief mode

--- static/js/inm.js
+++ static/js/inm.js
@@ -1,6 +1,7 @@
 "use strict";
 window.inm=$.extend(window.inm||{}, {
-    results: [], can:false, started: false, eqL:"", eqR: "114514", eqLim: "114"
+    results: [], can:false, started: false, eqL:"", eqR: "114514", eqLim: "114",
+    showAll: true
 });
 inm.translationDict = {
     "BigPrime": "19260817",
@@ -14,13 +15,21 @@ inm.translate = function(s, pos=0){//Not effective
     return $.map(this.translationDict, (v, k) => [[k, v]]).reduce((x, a) => x.replace(new RegExp(a[0^pos], "gi"), a[1^pos]), s);
 }
 inm.calculate = function(sl, sr) {
+    let mergeFunc = [this.dpBriefProve, this.dpProve][this.showAll+0].bind(this);
     this.results = [];this.can=0;
     return (new Promise((resolve, reject) =>
         resolve([sl, sr].map(this.digest).map(x => x.length?x:[0]).map(xs => this.algo.enumExpression(xs)))
-    )).then(lr => this.dpProve(...lr));
+    )).then(lr => mergeFunc(...lr));
     //this.dpProve(...[sl, sr].map(this.digest).map(x => x.length?x:[0]).map(this.split))
 }
 inm.dpProve = (l, r) => l.forEach((val,k)=>(inm.can < inm.eqLim || 0 == inm.eqLim)&&r.has(k)&&val.forEach(v => r.get(k).forEach(x=>{if(inm.can < inm.eqLim || 0 == inm.eqLim){++inm.can; inm.results.push(`${v} == ${x}`)}})));
+inm.dpBriefProve = (l, r) => l.forEach((val, k) => {
+    if(r.has(k)){
+        let x = r.get(k);
+        inm.results.push(`${val[0]} == ${x[0]} == ${parseInt(k) == k?k:k.toFixed(2)}`);
+        inm.can += val.length * x.length;
+    }
+})
 inm.digest = s => [].map.call(s, x => parseInt(x)).filter(x=>x>=0);
 inm.selectText = function(text){
     if (document.body.createTextRange) {
@@ -41,18 +50,19 @@ inm.resizer = function () {
 };
 inm.zeroModal = () => $("#modal-zero").modal().modal("open");
 inm.startProve = function(skipLim){
-    if(!skipLim && inm.eqLim == 0){
+    if(!skipLim && inm.eqLim == 0 && this.showAll){
         return this.zeroModal();
     }
     let sl=inm.eqL, sr=inm.eqR;
-    this.setArgs(sl, sr, inm.eqLim);
+    this.setArgs(sl, sr, inm.showAll && inm.eqLim);
     this.timedProve(sl, sr);
 }
 inm.forceProve = function(l, r, m){
     if(l.l){
         r=l.r;l=l.l;
     }
-    this.eqL = l;this.eqR=r;this.eqLim=m;
+    this.eqL = l;this.eqR=r;
+    m=="brief"?(this.showAll=false):(this.eqLim=m, this.showAll = true);
     Materialize.updateTextFields();
     if(m==0) return this.zeroModal();
     this.timedProve(l, r);
@@ -74,11 +84,12 @@ inm.reset = function(){
     this.results = [], this.can = this.started = false
 }
 inm.setArgs = function(l, r, m){
-    [l, r, m] = [l, r, m].map(s => this.translate(s, 1));
+    [l, r, m] = [l, r, m].slice(0, 3 - (m == false)).map(s => this.translate(s, 1));
+    m = m || "brief";
     history.pushState({l,r}, "", `#prove/${l}/${r}/${m}`);
 }
 inm.getArgs = function(tar){
-    let ptn = /#prove\/(\d+)\/(\d+)\/(\d+)/i, res=ptn.exec(this.translate(tar||location.hash));
+    let ptn = /#prove\/(\d+)\/(\d+)\/(brief|\d+)/i, res=ptn.exec(this.translate(tar||location.hash));
     return res && res.length === 4 && res.slice(1);
 }
 $&&$(function(){
